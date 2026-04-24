8978c4f044ccffe7ce228496420458aef29091ef
Hyacinthe Chemasle <hyacinthechem@gmail.com>
2026-04-25 01:10:09 +1200

    Handle plugin admin and create window params

--- src/landing_monitor.c
+++ src/landing_monitor.c
@@ -7,6 +7,8 @@
 #include "XPLMProcessing.h"
 #include <stdio.h>
 #include "XPLMDisplay.h"
+#include <string.h>
+#include "XPLMUtilities.h"
 
 
 /*
@@ -69,5 +71,99 @@ static void get_result_colour(float fpm, float *red, float *green, float *blue) 
    /* Red */ if (fpm > -600){*red=1.0f; *green=0.1f; *blue=0.1f; return; }
 }
 
-/* Create flight loop call back which x plane uses*/
-static float flight_loop_callback(float seconds_since_last_call, float seconds_since_last_loop, int counter, void *refcon){}
+/* forward declarations (needed because flight_loop and draw_window are */
+static float flight_loop_callback(float, float, int, void *);
+static void draw_result_window(XPLMWindowID, void *);
+
+/*
+ * XPLUGINSTART
+ * Called once by X-Plane when plugin loads
+ * This is where we find datarefs and create window
+ * Source: SDK/CHeaders/XPLM/XPLMPLugin.h
+ */
+
+PLUGIN_API int XPluginStart(char *outName, char *outSig, char *outDesc) {
+    // Identify plugin
+    strcpy(outName, "Landing Rate Monitor");
+    strcpy(outSig, "com.hyacinthechemasle.landingrate");
+    strcpy(outDesc, "Shows your landing rate at touchdown");
+
+
+    /*
+     * Crucial part:
+     * find all datarefs by their string name
+     * XPLMFindDataRef returns a handle, not a value
+     * we call it once here and store handle for later use
+     * Source: SDK/CHeaders/XPLM/XPLMDataAccess.h - XPLMFindDataRef()
+     */
+
+    vertical_speed_ref = XPLMFindDataRef("sim/flightmodel/position/vh_ind_fpm");
+    on_ground_ref = XPLMFindDataRef("sim/flightmodel/failures/onground_any");
+    airspeed_ref = XPLMFindDataRef("sim/flightmodel/position/indicated_airspeed");
+    gforce_ref = XPLMFindDataRef("sim/flightmodel/forces/g_nrml");
+
+    if (!vertical_speed_ref || !on_ground_ref || !airspeed_ref || !gforce_ref) {
+        /*XPLM Debug string write to the X-Plane 12 log.txt*/
+        XPLMDebugString("Landing Rate Monitor Failed to Load");
+        return 0; //on failure
+    }
+
+    /* Otherwise create the window for success, however we want to keep this hidden at first since we only want window
+     * to pop up when a landing is detected
+     *
+     */
+    XPLMCreateWindow_t window_params;
+    memset(&window_params, 0, sizeof(window_params));
+    window_params.structSize = sizeof(window_params);
+    window_params.left = 100; // pixels from left
+    window_params.top = 600; // pixels from top
+    window_params.right = 400; // pixels from right
+    window_params.bottom = 450; // pixels from bottom
+    window_params.visible = 0; // hidden at startup
+    window_params.drawWindowFunc = draw_result_window;
+    window_params.handleMouseClickFunc     = NULL;
+    window_params.handleKeyFunc            = NULL;
+    window_params.handleCursorFunc         = NULL;
+    window_params.handleMouseWheelFunc     = NULL;
+    window_params.refcon                   = NULL;
+    window_params.layer = xplm_WindowLayerFloatingWindows;
+    window_params.decorateAsFloatingWindow = xplm_WindowDecorationRoundRectangle;
+
+    result_window = XPLMCreateWindowEx(&window_params);
+    XPLMSetWindowPositioningMode(result_window, xplm_WindowPositionFree, -1);
+    XPLMSetWindowTitle(result_window, "Landing Rate");
+
+    XPLMDebugString("LandingRate: loaded successfully\n");
+    return 1; //indicate success
+}
+
+/*
+ * XPLUGINENABLE
+ * call when plugin is enabled
+ * this is where we can register our flight loop
+ * Source: SDK/CHeaders/XPLM/XPLMPlugin.h
+ */
+
+PLUGIN_API int XPluginEnable(void) {
+    /* Register flight loop to run every 0.1 seconds (100ms) */
+    XPLMRegisterFlightLoopCallback(flight_loop_callback, 0.1f, NULL);
+    return 1;
+}
+
+/* Called when plugin is disabled or X-Plane quits, important to clean up things you created */
+PLUGIN_API void XPluginDisable(void) {
+    XPLMUnregisterFlightLoopCallback(flight_loop_callback, NULL);
+}
+
+PLUGIN_API void XPluginStop() {
+    // Destroy window to free memory
+    if (result_window) {
+        XPLMDestroyWindow(result_window);
+        result_window = NULL;
+    }
+}
+
+/* Required by SDK but don't for plugin*/
+
+PLUGIN_API void XPluginReceiveMessage(XPLMPluginID from_plugin, int message, void *param){}
+
